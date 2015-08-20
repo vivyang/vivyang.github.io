e492848594d760454ff897e26fcb25ccdd3106a1
Vivian Yang <yangv473@gmail.com>
2015-08-20 12:30:22 -0400

    revised inside panels

--- trial/test.js
+++ trial/test.js
@@ -1,12 +1,26 @@
-$(".process_button").click(function(){
-    $('.red').addClass('show');
+// $(".process_button").click(function(){
+//     $('.red').addClass('show');
+// });
+$( ".process_button" ).hover(function(){
+  		$('.shade').addClass('show_shade');
 });
 $('.process_button').click(function() {
-    if ($(this).text() === 'Process') {
-         $(this).text('Hide');
+    if ($(this).text() === '+') {
+         $(this).text('-');
+         $('.red').addClass('show');
+         $('.blue').addClass('smaller');
+         $('.column-wrapperm').addClass('wrap');
     }
     else {
-        $(this).text('Process');
+        $(this).text('+');
         $('.red').removeClass('show');
+        $('.blue').removeClass('smaller');
+        $('.column-wrapperm').removeClass('wrap');
     }
 });
+// $( ".process_button" ).click(function() {
+//   $( "#block2" )
+//     .animate({ width: "90%" }, 1000 )
+//     .animate({ fontSize: "24px" }, 1000 )
+//     .animate({ borderLeftWidth: "15px" }, 1000 );
+// });
